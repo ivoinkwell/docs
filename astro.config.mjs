7feab3f68049665672de971bc1f09db313189b6f
ivoinkwell <1552965545@qq.com>
2026-09-16 20:16:46 +0800

    更新一篇文章

--- astro.config.mjs
+++ astro.config.mjs
@@ -128,6 +128,7 @@ export default defineConfig({
 						{ label: '00 - 宿舍管理系统环境搭建部署', link: '/software-testing-study-notes/00-dormitory-management-system-environment-setup-and-deployment' },
 						{ label: '01 - 根据需求说明书编写测试用例', link: '/software-testing-study-notes/01-write-test-cases-based-on-the-requirements-specification/' },
 						{ label: '02 - 利用等价类划分编写测试用例', link: '/software-testing-study-notes/02-write-test-cases-using-equivalence-class-division/' },
+						{ label: '03 - 利用边界值划分法设计测试用例', link: '/software-testing-study-notes/03-test-cases-are-designed-using-the-boundary-value-subdivision-method/' },
 					],
 				},
 				{
